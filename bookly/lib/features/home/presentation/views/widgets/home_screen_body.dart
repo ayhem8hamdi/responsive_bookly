e3b90478d336bde0782ff4027fc1f61dea998d10
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-06-09 17:52:45 +0100

    Add BestSeller Title & Code Refactor

--- bookly/lib/features/home/presentation/views/widgets/home_screen_body.dart
+++ bookly/lib/features/home/presentation/views/widgets/home_screen_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/core/utils/app_styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_item_list.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly/features/home/presentation/views/widgets/gap.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreenBody extends StatelessWidget {
@@ -9,21 +11,44 @@ class HomeScreenBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
-        const SliverToBoxAdapter(
-          child: Padding(
-            padding: EdgeInsets.only(top: 40, left: 30, right: 30, bottom: 45),
-            child: CustomAppBar(),
-          ),
-        ),
+        const CustomScrollViewAppBar(),
+        customScrollViewHorizontalBookList(context),
+        const Gap(25),
         SliverToBoxAdapter(
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height * 0.28 < 150
-                ? 150
-                : MediaQuery.of(context).size.height * 0.28,
-            child: const BookItemList(),
+          child: Text(
+            'Best Seller',
+            style: AppStyles.styleRegular18,
           ),
         ),
+        const Gap(18)
       ],
+    );
+  }
+
+  SliverToBoxAdapter customScrollViewHorizontalBookList(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.28 < 150
+            ? 150
+            : MediaQuery.of(context).size.height * 0.28,
+        child: const BookItemList(),
+      ),
+    );
+  }
+}
+
+class CustomScrollViewAppBar extends StatelessWidget {
+  const CustomScrollViewAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SliverToBoxAdapter(
+      child: Padding(
+        padding: EdgeInsets.only(top: 40, left: 30, right: 30, bottom: 45),
+        child: CustomAppBar(),
+      ),
     );
   }
 }
