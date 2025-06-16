a775fc313c7021c29deec36044b2aa064378cf71
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-06-16 17:05:03 +0100

    Code Refactor

--- bookly/lib/features/home/presentation/views/widgets/book_details_body.dart
+++ bookly/lib/features/home/presentation/views/widgets/book_details_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly/core/utils/app_styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_details_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_details_image.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_details_upper_section.dart';
+import 'package:bookly/features/home/presentation/views/widgets/pricing_row.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsBody extends StatelessWidget {
@@ -16,68 +17,6 @@ class BookDetailsBody extends StatelessWidget {
         BookDetailsUpperSection(),
         PricingRow()
       ],
-    );
-  }
-}
-
-class PricingRow extends StatelessWidget {
-  const PricingRow({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 35),
-        child: Row(
-          children: [
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(8),
-                        bottomLeft: Radius.circular(8))),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 15, horizontal: 3),
-                      child: Text(
-                        '19.99',
-                        style: AppStyles.styleRegular18
-                            .copyWith(color: const Color(0XFF000000)),
-                      ),
-                    ),
-                    Text(
-                      '£',
-                      style: AppStyles.styleRegular18.copyWith(
-                          color: const Color(0XFF000000), fontSize: 15),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                    color: Color(0XFFEF8262),
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(8),
-                        bottomRight: Radius.circular(8))),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 15),
-                  child: Text(
-                    'Free preview',
-                    style: AppStyles.styleRegular18,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ bookly/lib/features/home/presentation/views/widgets/pricing_row.dart
@@ -0,0 +1,76 @@
+import 'package:bookly/core/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class PricingRow extends StatelessWidget {
+  const PricingRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SliverToBoxAdapter(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 35),
+        child: Row(
+          children: [LeftPricingWidget(), RightPricingWidget()],
+        ),
+      ),
+    );
+  }
+}
+
+class LeftPricingWidget extends StatelessWidget {
+  const LeftPricingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(8), bottomLeft: Radius.circular(8))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 3),
+              child: Text(
+                '19.99',
+                style: AppStyles.styleRegular18
+                    .copyWith(color: const Color(0XFF000000)),
+              ),
+            ),
+            Text(
+              '£',
+              style: AppStyles.styleRegular18
+                  .copyWith(color: const Color(0XFF000000), fontSize: 15),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RightPricingWidget extends StatelessWidget {
+  const RightPricingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: const BoxDecoration(
+            color: Color(0XFFEF8262),
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(8), bottomRight: Radius.circular(8))),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15),
+          child: Text(
+            'Free preview',
+            style: AppStyles.styleRegular18,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
