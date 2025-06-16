e78c88c65d222d4e44f866c49e0d42d2ce27b8fe
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-06-16 17:39:05 +0100

    Done UI

--- bookly/lib/features/home/presentation/views/widgets/book_details_body.dart
+++ bookly/lib/features/home/presentation/views/widgets/book_details_body.dart
@@ -1,8 +1,7 @@
-import 'package:bookly/core/utils/app_styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_details_app_bar.dart';
+import 'package:bookly/features/home/presentation/views/widgets/book_details_bottom_section.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_details_image.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_details_upper_section.dart';
-import 'package:bookly/features/home/presentation/views/widgets/book_item_list.dart';
 import 'package:bookly/features/home/presentation/views/widgets/pricing_row.dart';
 import 'package:flutter/material.dart';
 
@@ -20,36 +19,5 @@ class BookDetailsBody extends StatelessWidget {
         BookDetailsBottomSection()
       ],
     );
-  }
-}
-
-class BookDetailsBottomSection extends StatelessWidget {
-  const BookDetailsBottomSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-        child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Text('You can also like',
-              style: AppStyles.styleRegular14.copyWith(color: Colors.white)),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.20 < 115
-              ? 105
-              : MediaQuery.of(context).size.height * 0.20,
-          child: const BookItemList(),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-      ],
-    ));
   }
 }
---
+++ bookly/lib/features/home/presentation/views/widgets/book_details_bottom_section.dart
@@ -0,0 +1,52 @@
+import 'package:bookly/core/utils/app_styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/book_item_list.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsBottomSection extends StatelessWidget {
+  const BookDetailsBottomSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SliverToBoxAdapter(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        BottomSectionTitle(),
+        SizedBox(
+          height: 20,
+        ),
+        BottomSectionListViewHoriz(),
+        SizedBox(
+          height: 20,
+        ),
+      ],
+    ));
+  }
+}
+
+class BottomSectionTitle extends StatelessWidget {
+  const BottomSectionTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Text('You can also like',
+          style: AppStyles.styleRegular14.copyWith(color: Colors.white)),
+    );
+  }
+}
+
+class BottomSectionListViewHoriz extends StatelessWidget {
+  const BottomSectionListViewHoriz({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.20 < 115
+          ? 105
+          : MediaQuery.of(context).size.height * 0.20,
+      child: const BookItemList(),
+    );
+  }
+}
