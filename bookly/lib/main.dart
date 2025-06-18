e0092a9d02a95588438393876f1889342099e1e3
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-06-18 02:45:15 +0100

    Add MultiBloc Provider

--- bookly/lib/main.dart
+++ bookly/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:bookly/core/utils/app_colors.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_locator.dart';
+import 'package:bookly/features/home/data/repos/home_repo_implementation.dart';
+import 'package:bookly/features/home/presentation/view_model/best_seller_cubit/cubit/best_seller_cubit.dart';
+import 'package:bookly/features/home/presentation/view_model/featured_books_cubit/featured_books_cubit.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 
 void main() {
+  setup();
   runApp(
     DevicePreview(
       enabled: true,
@@ -18,12 +24,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      theme: ThemeData().copyWith(
-        scaffoldBackgroundColor: AppColors.kBgColor,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) =>
+                FeaturedBooksCubit(getIt.get<HomeRepoImplementation>())),
+        BlocProvider(
+            create: (context) =>
+                BestSellerCubit(getIt.get<HomeRepoImplementation>()))
+      ],
+      child: GetMaterialApp(
+        theme: ThemeData().copyWith(
+          scaffoldBackgroundColor: AppColors.kBgColor,
+        ),
+        debugShowCheckedModeBanner: false,
+        getPages: AppRouter.getViews(),
       ),
-      debugShowCheckedModeBanner: false,
-      getPages: AppRouter.getViews(),
     );
   }
 }
