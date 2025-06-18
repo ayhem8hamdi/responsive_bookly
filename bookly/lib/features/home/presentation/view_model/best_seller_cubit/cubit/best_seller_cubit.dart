950c0dd561ea6c6e6bc07b3c8551b396c837d7ad
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-06-18 02:11:04 +0100

    Create Best Seller Cubit Function

--- bookly/lib/features/home/presentation/view_model/best_seller_cubit/cubit/best_seller_cubit.dart
+++ bookly/lib/features/home/presentation/view_model/best_seller_cubit/cubit/best_seller_cubit.dart
@@ -1,10 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/core/Errors/failure.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model/item.dart';
+import 'package:bookly/features/home/data/repos/home_repo_implementation.dart';
 import 'package:meta/meta.dart';
-
 part 'best_seller_state.dart';
 
 class BestSellerCubit extends Cubit<BestSellerState> {
-  BestSellerCubit() : super(BestSellerInitial());
+  BestSellerCubit(this.homeRepoImplementation) : super(BestSellerInitial());
+  final HomeRepoImplementation homeRepoImplementation;
+
+  Future<void> fetchBestSellerBooks() async {
+    emit(BestSellerLoading());
+    var result = await homeRepoImplementation.fetchFeaturedBooks();
+    result.fold((failure) => emit(BestSellerFailed(failure)),
+        (books) => emit(BestSellerSucces(books)));
+  }
 }
