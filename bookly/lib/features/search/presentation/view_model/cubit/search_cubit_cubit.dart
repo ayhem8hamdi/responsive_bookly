cfb48bb1b9bb0ed427d19e3aefd783a0ade6690c
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-06-20 01:38:48 +0100

    Done Search Cubit

--- bookly/lib/features/search/presentation/view_model/cubit/search_cubit_cubit.dart
+++ bookly/lib/features/search/presentation/view_model/cubit/search_cubit_cubit.dart
@@ -1,36 +1,32 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/core/Errors/failure.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model/item.dart';
-import 'package:meta/meta.dart';
+import 'package:bookly/features/search/data/SearchRepo/search_repo_impl.dart';
+import 'package:flutter/material.dart';
 
 part 'search_cubit_state.dart';
 
-/*class SearchCubitCubit extends Cubit<SearchCubitState> {
-  SearchCubitCubit() : super(SearchCubitInitial());
-}
-class SearchCubit extends Cubit<SearchCubitState> {
-  SearchCubit(this.homeRepoImplementation)
-      : super(SearchCubitInitial());
-  final HomeRepoImplementation homeRepoImplementation;
+class SearchCubitCubit extends Cubit<SearchCubitState> {
+  SearchCubitCubit(this.searchRepoImpl) : super(SearchCubitInitial());
+  final SearchRepoImpl searchRepoImpl;
 
-  Future<void> fetchFeauturedBooks() async {
-    emit(FeaturedBooksLoading());
-    final result = await homeRepoImplementation.fetchFeaturedBooks();
+  Future<void> fetchSearchedBooks({required String bookName}) async {
+    emit((SearchCubitLoading()));
+    final result = await searchRepoImpl.fetchSearchedBooks(bookName: bookName);
 
     result.fold(
       (failure) {
         debugPrint('FeaturedBooks Error: ${failure.message}');
-        emit(FeaturedBooksFailed(failure));
+        emit(SearchCubitFailure(failure));
       },
       (books) {
         final hasValidBooks =
             books.any((b) => b.volumeInfo?.getDisplayImageUrl() != null);
         emit(hasValidBooks
-            ? FeaturedBooksSucces(books)
-            : FeaturedBooksFailed(ImageProcessingFailure(
+            ? SearchCubitSucces(books)
+            : SearchCubitFailure(ImageProcessingFailure(
                 message: 'No valid book images found', code: 'NO_IMAGES')));
       },
     );
   }
 }
-*/
