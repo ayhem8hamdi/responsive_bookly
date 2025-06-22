564ab693637b629c1de93f78d6c5afe8b29647a7
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-06-22 18:16:00 +0100

    Make Searched Books Clickable

--- bookly/lib/features/search/presentation/views/widgets/searched_books_list.dart
+++ bookly/lib/features/search/presentation/views/widgets/searched_books_list.dart
@@ -1,6 +1,10 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model/volume_info.dart';
 import 'package:bookly/features/home/presentation/views/widgets/best_seller_item.dart';
+import 'package:bookly/features/search/presentation/view_model/cubit/search_cubit_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
 
 class SearchedBookSliverList extends StatelessWidget {
   const SearchedBookSliverList({super.key, required this.volumeInfo});
@@ -10,7 +14,15 @@ class SearchedBookSliverList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverList(
       delegate: SliverChildBuilderDelegate(
-        (context, index) => BestSellerItem(volumeInfo: volumeInfo[index]),
+        (context, index) => GestureDetector(
+            onTap: () {
+              context.read<SearchCubitCubit>().fetchYouMayLikeBooks(
+                    selectedBook: volumeInfo[index],
+                  );
+              Get.toNamed(AppRouter.bookDetailsScreen,
+                  arguments: volumeInfo[index]);
+            },
+            child: BestSellerItem(volumeInfo: volumeInfo[index])),
         childCount: volumeInfo.length,
       ),
     );
